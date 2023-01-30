0a967e036d3fd9db009fd9fc3b9a81e88a895424
soumya-2911 <118664555+soumya-2911@users.noreply.github.com>
2023-01-31 01:24:49 +0530

    Add files via upload

--- ml_test.py
+++ ml_test.py
@@ -1,40 +1,58 @@
+
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #initialization
 
-df=pd.read_excel("D:\SOUMYA\ml\Book1.xlsx")
-x=np.transpose([df['area']/100,df['bedroom'],df['kitchen'],df['comfort']/100])
-y=np.transpose(df['price'])
+df=pd.read_excel("Emissions_train.xlsx")
+
+x=np.transpose([df['Coal'],df['Oil'],df['Gas'],df['Per Capita']*10])
+
+y=np.array(df['Total'])
+
+#plt.scatter(np.transpose(x)[0],y)
+#plt.show()
 
 
-
-
-def train(x,y_label,iter,learning_rate):
+def train(x,y,iter,learning_rate):
     weight=np.zeros(len(x[0]))
     bias=0
-    features=len(weight)
+    
     m=len(y)
     for iteration in range(iter):
 
         
+        #print(weight)
+        diff_vector_w=(np.dot(x,weight)+bias-y)/m
         
-        slope_w=np.dot((np.dot(x,weight)+bias-y),(np.sum(x,axis=1)))
-        weight=weight-learning_rate*slope_w
+        
         
         slope_b=np.sum((np.dot(x,weight)+bias-y))/m
+
+        for j in range(len(weight)):
+
+            weight[j]=weight[j]-learning_rate*np.dot(diff_vector_w,np.transpose(x)[j])
+            bias=bias-learning_rate*slope_b
         print(slope_b)
-        bias=bias-learning_rate*slope_b
 
     return weight,bias
 
+
+    
+
 #training
-weight,bias=train(x,y,150,0.0001)
+weights,biases=train(x,y,400,0.000001)
 
 #prediction
-df_test=pd.read_excel("D:\SOUMYA\ml\Book2.xlsx")
-x_test=np.transpose([df_test['area']/100,df_test['bedroom'],df_test['kitchen'],df_test['comfort']/100])
+df_test=pd.read_excel("Emissions_test.xlsx")
+x_test=np.transpose([df_test['Coal'],df_test['Oil'],df_test['Gas'],df_test['Per Capita']*10])
 
-price=(np.dot(x_test,weight))+bias
+total=(np.dot(x_test,weights))+biases
 print('------------')
-print(price)
+
+#plt.scatter(np.transpose(x_test)[0],p)
+#plt.show()
+
+print(df_test['Country'])
+print(total)
